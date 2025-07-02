23214f8166dccdd7381703b2712050ccf05b0d11
Lelebanimes00 <94663940+CORINGA88hacker@users.noreply.github.com>
2025-07-01 22:36:41 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Substitua pelos dados reais do seu projeto Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  databaseURL: "SUA_DATABASE_URL",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
